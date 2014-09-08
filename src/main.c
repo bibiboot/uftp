ec80edcd1305d6cac82f2dfe9df417ec2466a026
dan <rehman.d.fazlur@gmail.com>
2014-09-08 01:47:48 -0700

    Adding the hashmap test code

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include "globals.h"
+#include "hashl.h"
+
+void test_hashmap(){
+    void *chunk_ptr = malloc(sizeof(100));
+    strcpy(chunk_ptr, "Hello");
+    hashed_link h = { .seq_num = 1 , .dl_ptr = chunk_ptr  };
+    add_hashl(&h);
+
+    hashed_link *result = (hashed_link *)(find_hashl(1));
+    printf("Name = %s\n", (result->dl_ptr));
+}
+
+void test_global(){
+    printf("PORT = %d\n", globals.config.port);
+}
 
 int main(int argc, char *argv[]){
-    // FATAL("Testing");
-    printf("PORT = %d\n", globals.config.port);
-    return 0;
-
+    test_global();
+    test_hashmap();
 }
