45245983c6271feb7d4e7ba10f0db1ce3d90172c
dan <rehman.d.fazlur@gmail.com>
2014-09-08 17:46:30 -0700

    Adding the main changes

--- src/main.c
+++ src/main.c
@@ -4,77 +4,22 @@
 #include "mmap.h"
 #include "realibility_list.h"
 
-void test_ll(){
-    My402ListElem *elem = NULL;
-    int *temp;
-    int i,j,k,l;
-
-    //List Initialization
-    if (My402ListInit(&globals.datal)==0){
-	printf("Initialization of the list failed");
-	return ;
-    }
-    //Creating the List
-    i=10;
-    j=20;
-    k=30;
-
-    if (My402ListAppend(&globals.datal , &i)==0)
-    {
-	printf("Append Failed");
-	return;
-    }
-
-    printf("\nNode Value :%d \n",i);
-    /*
-    if (My402ListAppend(&Q1 , &j)==0){
-        printf("Append Failed");
-        return ;
-    }
-    printf("\nNode Value :%d \n",j);
-
-    if (My402ListAppend(&Q1 , &k)==0){
-	printf("Append Failed");
-	return ;
-    }
-    printf("\nNode Value :%d \n",k);
-
-    //Deleting the list
-    for(l=0;l<3;l++)
-    {
-	elem = My402ListFirst(&Q1);
-	temp = elem-> obj;
-	printf("\nDeleted Value is %d", *temp);
-	My402ListUnlink(&Q1,elem);
-    }
-    printf("\n\n-----------------Program Ends----------------------\n");
-    */
-}
-
-void test_hashmap(){
-    void *chunk_ptr = malloc(sizeof(100));
-    strcpy(chunk_ptr, "Hello");
-    hashed_link h = { .seq_num = 1 , .dl_ptr = chunk_ptr  };
-    add_hashl(&h);
-
-    hashed_link *result = (hashed_link *)(find_hashl(1));
-    printf("Name = %s\n", (result->dl_ptr));
-}
-
-void test_global(){
-    printf("PORT = %d\n", globals.config.port);
-}
-
 int main(int argc, char *argv[]){
-    test_ll();
-    test_global();
-    test_hashmap();
 
-    // Test memory map files
-    char *filename = "etc/data/data.bin";
-    long long int total_file_size = 0;
-    char *data_ptr = get_memory_map_ptr(filename, &total_file_size);
+    // Create memory map files
+    char *data_ptr = get_memory_map_ptr(globals.config.filename, &globals.config.total_size);
 
-    // Test create list
-    create_data_list(total_file_size);
+    // Create data list
+    create_list(data_ptr, &globals.datal, DATA);
+
+    // Create nack list
+    create_list(data_ptr, &globals.nackl, NACK);
+
+    // Print data in packet
+    print_file_packet_wise(data_ptr);
+
+    // Print data list
+    print_list(&globals.datal);
+    // Print nace list
+    print_list(&globals.nackl);
 }
